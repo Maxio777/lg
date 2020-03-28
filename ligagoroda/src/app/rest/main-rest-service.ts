3c2f4795771b124adf06007c46eb1800bacfcd04
miossel <iossel.maxim@gmail.com>
2020-03-29 02:10:46 +0300

    api

--- ligagoroda/src/app/rest/main-rest-service.ts
+++ ligagoroda/src/app/rest/main-rest-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_DEV_LG = 'http://ligagoroda.ru/v1/';
+const API_DEV_LG = 'http://ligagoroda.ru/api/v1/';
 const OPTIONS = { headers: { 'Content-Type': 'application/json' }};
 
 
