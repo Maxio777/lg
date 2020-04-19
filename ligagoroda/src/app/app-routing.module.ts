a8981e72eccf45a1fb27b8227b77662783519119
miossel <iossel.maxim@gmail.com>
2020-04-19 18:40:29 +0300

    load path client module 2

--- ligagoroda/src/app/app-routing.module.ts
+++ ligagoroda/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {AuthGuardService, ChugunGuardService} from './services/guards/auth-guard
 const routes: Routes = [
   {path: 'admin', loadChildren: './modules/admin/admin.module#AdminModule', canActivate: [ChugunGuardService, AuthGuardService] },
   {path: 'auth', loadChildren: './modules/auth/auth.module#AuthModule' },
-  {path: '', loadChildren: './modules/auth/client.module#ClientModule' },
+  {path: '', loadChildren: './modules/client/client.module#ClientModule' },
 ];
 
 @NgModule({
